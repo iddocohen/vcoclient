644d2e964835a672ceea82ad28579d7a9d970406
Iddo Cohen <iddocohen@gmail.com>
2019-05-11 22:23:27 +0200

    First add

---
+++ vcoclient.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+
+#
+# vcoclient.py
+#
+# A simple VeloCloud Orchestrator (VCO) Python client
+# 
+# The idea, with simple shell and with this simple client, create complex workflows with VCO.
+# The client uses barebone VCO API and python libaries to be slim as possible.
+#
+# It uses argparse and functional hooks, to execute those. Each functional hook, has its help and one can use those to understand what is supported with client.
+#
+# Example:
+#  1) How synergy works:
+#
+#  ```
+#  ./vcoclient.py --vco=192.168.2.55 logout
+#  ./vcoclient.py --vco=192.168.2.55 login --username=super@velocloud.net --password=VeloCloud123
+#  ./vcoclient.py --vco=192.168.2.55 edges_get --search=VCE-Branch-1
+#  ./vcoclient.py --vco=192.168.2.55 edges_get
+#  ```
+#
+
+# For future Python 3 compatibility
+from __future__ import print_function
+
+# Needed libaries
+# TODO: Think about remove pandas
+import requests, pickle, json, re, argparse, os
+import pandas as pd
+from pandas.io.json import json_normalize
+
+# To be able to disable SSL warnings as needed e.g. development environment
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+VERIFY_SSL=False
+
+class ApiException(Exception):
+    pass
+
+class VcoRequestManager(object):
+
+    #TODO: Give path outside here for the user to alter
+    def __init__(self, hostname, verify_ssl=VERIFY_SSL, path="/tmp/"):
+        """
+        Init the Class
+        """
+        self._session = requests.Session()
+        self._verify_ssl = verify_ssl
+        if self._verify_ssl == False:
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        self._root_url = self._get_root_url(hostname)
+        self._portal_url = self._root_url + "/portal/"
+        self._livepull_url = self._root_url + "/livepull/liveData/"
+        self._store_cookie = path + hostname + ".txt"
+        self._seqno = 0
+
+    def _get_root_url(self, hostname):
+        """
+        Translate VCO hostname to a root url for API calls 
+        """
+        if hostname.startswith("http"):
+            re.sub('http(s)?://', '', hostname)
+        proto = "https://"
+        return proto + hostname
+
+    def authenticate(self, username="", password="", logout=False, is_operator=True):
+        """
+        Authenticate to API - on success, a cookie is stored in the session and file
+        """
+        if not logout:
+          path = "/login/operatorLogin" if is_operator else "/login/enterpriseLogin"
+          data = { "username": username, "password": password }
+        else:
+          path = "/logout"
+          data = {}
+
+        url = self._root_url + path
+        headers = { "Content-Type": "application/json" }
+        r = self._session.post(url, headers=headers, data=json.dumps(data),
+                               allow_redirects=True, verify=self._verify_ssl)
+
+        # TODO: Different status for "missing" string in cookie
+        #if r.status_code == 200 and "missing" not in self._session.cookies["velocloud.session"] and self._save_cookie():
+        if r.status_code == 200:
+            if not logout:
+                self._save_cookie()
+            else:
+                self._del_cookie()
+            return True
+        return False
+        
+         
+    def call_api(self, method, params):
+        """
+        Build and submit a request
+        Returns method result as a Python dictionary
+        """
+        if "velocloud.session" not in self._session.cookies: 
+            if not self._load_cookie():
+                raise ApiException("Cannot load session cookie") 
+        
+        self._seqno += 1
+        headers = { "Content-Type": "application/json" }
+        method = self._clean_method_name(method)
+        payload = { "jsonrpc": "2.0",
+                    "id": self._seqno,
+                    "method": method,
+                    "params": params }
+
+        if method in ("liveMode/readLiveData", "liveMode/requestLiveActions", "liveMode/clientExitLiveMode"):
+            url = self._livepull_url
+        else:
+            url = self._portal_url
+
+        r = self._session.post(url, headers=headers,
+                               data=json.dumps(payload), verify=self._verify_ssl)
+
+        response_dict = r.json()
+        if "error" in response_dict:
+            raise ApiException(response_dict["error"]["message"])
+        return response_dict["result"]
+
+    def _clean_method_name(self, raw_name):
+        """
+        Ensure method name is properly formatted prior to initiating request
+        """
+        return raw_name.strip("/")
+    
+    def _save_cookie(self):
+        """
+        Save cookie from VCO
+        """
+        with open(self._store_cookie, "wb") as f:
+            try:
+                pickle.dump(self._session.cookies, f)
+                return True
+            except:
+                return False	
+
+    def _load_cookie(self):
+        """
+        Load VCO session cookie
+        """
+        if not os.path.isfile(self._store_cookie):
+            return False
+        
+        with open(self._store_cookie, "rb") as f:
+            try:
+               self._session.cookies.update(pickle.load(f))
+               return True
+            except:
+               return False
+
+    def _del_cookie(self):
+        """
+        Delete VCO session cookie
+        """
+      
+        try: 
+           os.remove(self._store_cookie)
+        except:
+           raise ApiException("Cannot delete cookie file")
+
+def logout(args):
+    """
+    Logout from VCO
+    """ 
+    client = VcoRequestManager(args.hostname)
+    o = client.authenticate(logout=True) 
+    print(o)
+    
+
+def login (args):
+    """
+    Login at VCO
+    """
+    client = VcoRequestManager(args.hostname)
+    o = client.authenticate(args.username, args.password, is_operator=args.operator)
+    print (o)
+        
+def edges_get (args):  
+    """
+    Gets edges from VCO.  
+    """
+    client = VcoRequestManager(args.hostname)
+    o = client.call_api("enterprise/getEnterpriseEdges", { "enterpriseId": 1 })
+    j = json.loads(json.dumps(o))
+    df = pd.DataFrame.from_dict(json_normalize(j), orient='columns')
+    out = None
+    if "search" in args:
+      out = df[df['name'].str.contains(args.search)].T
+    else:
+      out = df.T
+
+    # TODO: Lets not convert it twice in a case JSON is chosen as default output
+    if args.output == "json":
+      out = out.T.to_json()
+
+    print(out)
+
+def sysprop_set (args):
+    """
+    Set system properties 
+    """
+    #TODO: There must be a better way but Namespace object does not have .copy() or .remove() 
+    payload = {}
+    for k in vars(args):
+      payload[k] = getattr(args, k)
+    
+    del payload["func"]
+    del payload["hostname"]
+    del payload["output"]
+
+    client = VcoRequestManager(args.hostname)
+    o = client.call_api("systemProperty/insertOrUpdateSystemProperty", payload)
+    if "rows" in o:
+      print("True")
+    else:
+      print(o)
+    
+
+if __name__ == "__main__":
+    """
+    Based on the arguements provided, it will execute a given function
+    """
+    parser = argparse.ArgumentParser(description="A simple VeloCloud Orchestrator (VCO) client via Python")
+    parser.add_argument("--vco", action="store", type=str, dest="hostname",
+                        help="Hostname/IP of VCO")
+    parser.add_argument("--output", action="store", type=str, dest="output", default="pandas", choices=["pandas", "json"],
+                        help="Pandas tables are used as default output method but one can also use 'json'")
+
+    subparsers = parser.add_subparsers()
+
+    # Login function
+    parser_login = subparsers.add_parser("login")
+    parser_login.add_argument("--username", action="store", type=str, dest="username", required=True,
+                              help="Username for Authentication")
+
+    parser_login.add_argument("--password", action="store", type=str, dest="password", default="",
+                              help="Password for Authentication")
+    
+    parser_login.add_argument("--operator", action="store_true", dest="operator", default=True,
+                              help="Per default we login as operator to VCO. If not, one can set this to false.")
+
+    parser_login.set_defaults(func=login)
+    
+    # Logout function
+    parser_logout = subparsers.add_parser("logout")
+    parser_logout.set_defaults(func=logout)
+
+
+    # Get all Edges from Operator View
+    parser_getedges = subparsers.add_parser("edges_get")
+    
+    parser_getedges.add_argument("--search", action="store", type=str, dest="search", 
+                              help="Search Edge/Edges containing the given name")
+    
+    parser_getedges.set_defaults(func=edges_get)
+
+    # Update/insert system properties in VCO
+
+    parser_sysprop_set = subparsers.add_parser("sysprop_set")
+    
+    parser_sysprop_set.add_argument("--name", action="store", type=str, dest="name", required=True, 
+                              help="Name of the new/edit system property")
+
+    parser_sysprop_set.add_argument("--value", action="store", type=str, dest="value", required=True, 
+                              help="New value of the system property")
+    
+    parser_sysprop_set.set_defaults(func=sysprop_set)
+
+
+
+    args = parser.parse_args()
+    args.func(args)
+
