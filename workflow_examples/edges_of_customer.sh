e58982ac409741d84045063cf9937088080c6627
Iddo Cohen <iddocohen@gmail.com>
2019-06-29 21:31:48 +0200

    Fixing erroring out of bash

--- workflow_examples/edges_of_customer.sh
+++ workflow_examples/edges_of_customer.sh
@@ -10,14 +10,14 @@ RET=$(vcoclient.py login --no-operator)
 
 if [ $? -eq 1 ]; then
     echo $RET
-    exit
+    exit 1
 fi
 
 # Trying to get any random customer for MSP from VCO
 ID=$(vcoclient.py --output=json msp_customers_get | jq '.[].id' | shuf | head -n 1 )
 if [ $? -eq 1 ] && [ ${#ID} > 0 ]; then
     echo $ID
-    exit
+    exit 1
 fi
 
 # Executing the edges_get method 
@@ -27,7 +27,7 @@ RET=$(vcoclient.py --output=csv edges_get --search=* --filters=interface --id=$I
 # Checking that we do not have an error
 
 if [ $? -eq 1 ]; then
-    exit
+    exit 1
 else
     echo $RET
 fi
@@ -40,7 +40,7 @@ RET=$(vcoclient.py logout)
 
 if [ $? -eq 1 ]; then
     echo $RET
-    exit
+    exit 1
 fi
 
 
